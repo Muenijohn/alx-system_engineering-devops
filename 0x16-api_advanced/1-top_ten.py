0cbed13e5e908a6b1f4666692242aac439cf1603
muenijohn <muenijohn592@gmail.com>
2023-05-09 18:48:28 +0000

    prints top 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
+    """Print the titles of the 10 hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
